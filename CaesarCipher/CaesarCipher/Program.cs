1c86705406df80fda415ee3243440a7350387e96
Jasmine Meade <jmeade4347@gmail.com>
2020-10-28 00:25:40 -0400

    Finished the project

--- CaesarCipher/CaesarCipher/Program.cs
+++ CaesarCipher/CaesarCipher/Program.cs
@@ -6,7 +6,26 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            
+            char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l', 'm', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+            Console.WriteLine("enter a super secret password: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+
+            char[] secretMessage = input.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
+
+            for (int i = 0; i < secretMessage.Length; i++) 
+            {
+                char letter = secretMessage[i];
+                int letterPosition = Array.IndexOf(alphabet, letter);
+                int newLetterPosition = (letterPosition + 3) % 26;
+                char letterEncoded = alphabet[newLetterPosition];
+                encryptedMessage[i] = letterEncoded;
+            }
+            string encodedString = String.Join("", encryptedMessage);
+            Console.WriteLine($"Your encoded message is: {encodedString}");
         }
     }
 }
